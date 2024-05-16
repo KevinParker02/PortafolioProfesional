48a465961c29f475f37ce2e8c2a4104de8d775b6
KevinVivanco_02 <131885520+KevinParker02@users.noreply.github.com>
2024-05-16 09:09:29 -0400

    Primera carga del Potafolio
    El primer avance contempla es esqueleto de la página, contiene:
    - NavBar (Que te dirige a alguna sección).
    - secciones principales.
    - Algunos estilos (CSS)
    - API GitHub (JS).
    - Validación formulario (JS).

---
+++ assets/js/scripts.js
@@ -0,0 +1,93 @@
+function obtenerDatosUsuario(username) {
+    const userUrl = `https://api.github.com/users/${username}`;
+
+    fetch(userUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('No se pudo obtener los datos del usuario.');
+            }
+            return response.json();
+        })
+        .then(userData => {
+            console.log(userData);
+
+            // Actualizar el contenido de HTML con los datos del usuario.
+            document.getElementById('nombre').textContent = userData.name || 'Nombre no disponible';
+            document.getElementById('bio').textContent = userData.bio || 'Biografía no disponible';
+            document.getElementById('seg').textContent = userData.followers;
+            document.getElementById('repository').textContent = userData.public_repos;
+
+            // Obtener la foto de perfil (avatar)
+            const avatarUrl = userData.avatar_url;
+
+            // Mostrar la foto de perfil
+            const avatarImage = document.getElementById('avatar');
+            avatarImage.src = avatarUrl; // Asignar la URL del avatar al src
+            avatarImage.alt = `${username}'s avatar`;
+        })
+        .catch(error => {
+            console.log('Error al obtener datos del usuario: ', error);
+        });
+}
+
+// Invocar la función con el nombre de usuario
+obtenerDatosUsuario('KevinParker02');
+
+
+// --------------------------------------------------- Obtener datos del formulario.
+let nombre = document.getElementById("nombre");
+let nombreERROR = document.getElementById("nombreERROR");
+
+let Recado = document.getElementById("Recado");
+
+let Email = document.getElementById("Email");
+let correoERROR = document.getElementById("correoERROR");
+
+// Para el formulario (Agarra todo el contenido)
+let form = document.getElementById("myForm");
+
+// Prevenimos que se envíe el formulario con campos sin rellenar
+form.addEventListener("submit", function(){
+    event.preventDefault(); //Prevenir recarga automática del formulario.
+    validarFormulario();
+});
+
+//Validamos los datos del formulario
+function validarFormulario(){
+    //Eliminamos los textos de error
+    nombreERROR.textContent="";
+    correoERROR.textContent="";
+
+    //Validamos los inputs
+    if(nombre.value === null|| nombre.value.trim()===""){
+        nombreERROR.textContent="Por favor ingrese su nombre."
+    };
+
+    //Validamos el Email
+    const inputValue2 = Email.value.trim();
+    const allowedChars2 = /[a-zA-Z@ . 0-9\ _ ]/;
+        //sanitizedValu2 (Variable) obtendrá el valor depurado, es decir, que una vez ingrese en el ciclo ELIMINARA cualquier caracter que no sea válido.
+    let sanitizedValu2 = '';
+    for (let i = 0; i < inputValue2.length; i++) {
+        if (allowedChars2.test(inputValue2[i])) {
+            sanitizedValu2 += inputValue2[i];
+        }
+    };
+        //Finalmente actualizamos el registro obtenido en el imput y lo comparamos en los IF
+        Email.value = sanitizedValu2;
+
+        if (inputValue2 === "") {
+        correoERROR.textContent = "Debe ingresar un correo.";
+    } else if (inputValue2 !== sanitizedValu2) {
+        correoERROR.textContent = "Debe ingresar un correo válido.";
+    } else {
+        correoERROR.textContent = "";
+    };
+
+    if(nombreERROR.textContent==="" && rutERROR.textContent==="" && genError.textContent==="" && correoERROR.textContent===""){
+        //Código para enviar un formulario.
+        alert("El formulario se envió correctamente.");
+        form.submit();
+    };
+
+};
